ee5d1aec70a44be611cf43ed10374ff0dfee8a92
Jose Mar?a Alvarez <chema.ar@gmail.com>
2012-08-21 12:24:14 +0200

    Structuring script

--- trunk/prototypes/rocas-hadoop/run.sh
+++ trunk/prototypes/rocas-hadoop/run.sh
@@ -4,6 +4,7 @@ artifactId=`grep artifactId pom.xml | head -1 | cut -d">" -f2 | cut -d"<" -f1`
 version=`grep version pom.xml | head -1 | cut -d">" -f2 | cut -d"<" -f1`
 input="input"
 output="output"
+
 #Compiling and packaging
 mvn -o clean
 mvn -o assembly:assembly -Dmaven.test.skip=true
